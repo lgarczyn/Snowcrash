ec97a3877137572dc6f219ef5edab343180f1f15
lgarczyn <lgarczyn@student.42.fr>
2019-04-03 18:28:29 +0200

    added explenation of pearl script

--- level12/Ressources/level12.pl
+++ level12/Ressources/level12.pl
@@ -3,21 +3,23 @@
 use CGI qw{param};
 print "Content-type: text/html\n\n";
 
+# modify first argument, then for every match of first argument inside /tmp/xd, compare to second arg
 sub t {
   $nn = $_[1];
-  $xx = $_[0];
-  $xx =~ tr/a-z/A-Z/; 
-  $xx =~ s/\s.*//;
-  @output = `egrep "^$xx" /tmp/xd 2>&1`;
-  foreach $line (@output) {
-      ($f, $s) = split(/:/, $line);
-      if($s =~ $nn) {
-          return 1;
+  $xx = $_[0]; # get first argument
+  $xx =~ tr/a-z/A-Z/; # capitalize every letter of first arg
+  $xx =~ s/\s.*//; # delete anything following a whitespace character of first arg
+  @output = `egrep "^$xx" /tmp/xd 2>&1`; # find match of first arg inside /tmp/xd, keep errors in stdin
+  foreach $line (@output) { # for each match
+      ($f, $s) = split(/:/, $line); # get match 
+      if($s =~ $nn) { # if match equals second argument
+          return 1; # return true
       }
   }
   return 0;
 }
 
+#print .. if first arg is true
 sub n {
   if($_[0] == 1) {
       print("..");
@@ -26,4 +28,5 @@ sub n {
   }    
 }
 
+#get parram x and y, not sure how to give those param to the script
 n(t(param("x"), param("y")));
